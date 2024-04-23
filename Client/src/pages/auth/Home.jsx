e03fe4a059e148078ff57c252810fb510aa22c4b
Edinson <edisonprise@hotmail.com>
2024-04-22 20:15:00 -0500

    styles preparation in home component

--- Client/src/pages/auth/Home.jsx
+++ Client/src/pages/auth/Home.jsx
@@ -1,13 +1,21 @@
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
-import { useState } from "react";
+
 
 const Home = () => {
   return (
-    <div>
+    <>
+    <div  >
       <Navbar />
       <Sidebar />
     </div>
+      <div className=" flex flex-col items-center justify-center flex-grow">
+      <h1>Vehiculos Estacionados Hoy</h1>
+      <h1>Tarifas</h1>
+      <h1>Dinero en Caja</h1>
+      <h1>Abonados</h1>
+      </div>
+    </>
   );
 };
 
