1cec4d6df0f26063dd130b796f9f13253fee8b40
Edinson <edisonprise@hotmail.com>
2024-04-22 20:15:26 -0500

    saves

--- Client/src/pages/auth/Home.jsx
+++ Client/src/pages/auth/Home.jsx
@@ -1,19 +1,18 @@
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
 
-
 const Home = () => {
   return (
     <>
-    <div  >
-      <Navbar />
-      <Sidebar />
-    </div>
+      <div>
+        <Navbar />
+        <Sidebar />
+      </div>
       <div className=" flex flex-col items-center justify-center flex-grow">
-      <h1>Vehiculos Estacionados Hoy</h1>
-      <h1>Tarifas</h1>
-      <h1>Dinero en Caja</h1>
-      <h1>Abonados</h1>
+        <h1>Vehiculos Estacionados Hoy</h1>
+        <h1>Tarifas</h1>
+        <h1>Dinero en Caja</h1>
+        <h1>Abonados</h1>
       </div>
     </>
   );
