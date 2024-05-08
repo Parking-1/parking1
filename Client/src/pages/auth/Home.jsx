905538c7e6db43dca3e3d7bb53c8579585391ea7
Edinson <edisonprise@hotmail.com>
2024-05-07 20:14:23 -0500

    fix positions of boxes in home

--- Client/src/pages/auth/Home.jsx
+++ Client/src/pages/auth/Home.jsx
@@ -15,45 +15,64 @@ const Home = () => {
         <div className="flex flex-col my-160">
           <Sidebar />
         </div>
-        <div className="flex m-auto w-100 ">
-          <div className="grid grid-cols-4 gap-16">
-            <div className="text-2xl m-13 text-white  text-center border ">
-              <Link to="/ingresos" className="bg-blue-500 p-8">
-                Estacionados Hoy
-              </Link>
+        <div className="flex m-auto w-full ">
+          <div className="grid grid-cols-4 gap-10 ml-40">
+            <div className="text-2xl m-13 text-white text-center border">
+              <div className="flex flex-col items-center justify-center h-full">
+              <div className="w-full bg-blue-500 p-8">
+                <Link to="/ingresos" className="bg-blue-500 p-8">
+                  Estacionados Hoy
+                </Link>
+                </div>
+                <img
+                  src={Garage}
+                  className="max-h-40 min-w-60 max-w-60 m-auto"
+                  alt="Garage"
+                />
+              </div>
+            </div>
+            <div className="text-2xl m-1 text-center text-white border">
+              <div className="flex flex-col items-center justify-center h-full">
+                <div className="w-full bg-blue-500 p-8">
+                  <Link to="/tarifas" className=" ">
+                    Tarifas
+                  </Link>
+                </div>
 
-              <img
-                src={Garage}
-                className="max-h-40 min-w-60 max-w-60 m-auto"
-              ></img>
+                <img
+                  src={barras}
+                  className="max-h-40 min-w-60 max-w-60 m-auto"
+                  alt="Barras"
+                />
+              </div>
             </div>
-            <div className="text-2xl m-13  text-center  text-white border ">
-              <Link to="/tarifas" className="bg-blue-500 p-8">
-                Tarifas
-              </Link>
-              <img
-                src={barras}
-                className="max-h-40 m-auto min-w-60 max-w-60"
-              ></img>
+            <div className="text-2xl m-13 text-center text-white border">
+              <div className="flex flex-col items-center justify-center h-full">
+              <div className="w-full bg-blue-500 p-8">
+                <Link to="/caja" className="bg-blue-500 p-8">
+                  Dinero en Caja
+                </Link>
+                </div>
+                <img
+                  src={Cashbox}
+                  className="max-h-40 min-w-60 max-w-60 m-auto"
+                  alt="Cashbox"
+                />
+              </div>
             </div>
-            <div className="text-2xl m-13  text-center  text-white border">
-              <Link to="/caja" className="bg-blue-500 p-8">
-                Dinero en Caja
-              </Link>
-              <img
-                src={Cashbox}
-                className="max-h-40 m-auto min-w-60 max-w-60"
-              ></img>
-            </div>
-            <div className="text-2xl m-13  text-center text-white border">
-              <Link to="/abonados" className="bg-blue-500 p-8">
-                {" "}
-                Abonados
-              </Link>
-              <img
-                src={Team}
-                className="max-h-40 m-auto min-w-60 max-w-60"
-              ></img>
+            <div className="text-2xl m-13 text-center text-white border">
+              <div className="flex flex-col items-center justify-center h-full">
+              <div className="w-full bg-blue-500 p-8">
+                <Link to="/abonados" className="bg-blue-500 p-8">
+                  Abonados
+                </Link>
+                </div>
+                <img
+                  src={Team}
+                  className="max-h-40 min-w-60 max-w-60 m-auto"
+                  alt="Team"
+                />
+              </div>
             </div>
           </div>
         </div>
