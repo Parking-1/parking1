b9f83bc423ca1c3d3639c9dc3f3b6f478ffc7000
Edinson <edisonprise@hotmail.com>
2024-08-22 21:01:53 -0500

    redux implementation

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
-import store from "./redux/store.js";
+import { Provider } from "react-redux"
+import store from "./redux/store";
 
 import "react-toastify/dist/ReactToastify.css";
 
-import { Provider } from "react-redux"
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
